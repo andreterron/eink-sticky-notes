d54e97d24de92e4ffdf02a9223099e1af04c8bb9
Andre Terron <git@terron.me>
2025-02-25 00:26:35 -0800

    Test task design and using a bmp buffer

--- eink/magtag/src/code.py
+++ eink/magtag/src/code.py
@@ -10,13 +10,14 @@ import socketpool
 import adafruit_requests
 import displayio
 from adafruit_magtag.magtag import MagTag
-import adafruit_imageload.png as png_load
+# import adafruit_imageload.png as png_load
+import adafruit_imageload.bmp as bmp_load
 
 HOSTNAME = os.getenv("SERVER_HOSTNAME")
 
 # REST endpoints
 CHECK_JSON_URL = HOSTNAME + "/data"
-IMAGE_URL      = HOSTNAME + "/image.png"
+IMAGE_URL      = HOSTNAME + "/image.bmp"
 
 
 # Track the last update we saw
@@ -31,26 +32,13 @@ requests = adafruit_requests.Session(pool, ssl.create_default_context())
 
 magtag = MagTag()
 
-magtag.add_text(
-    text_position=(
-        50,
-        (magtag.graphics.display.height // 2) - 1,
-    ),
-    text_scale=3,
-)
-magtag.set_text("Hello World")
-
 print("Go!")
 timestamp = time.monotonic()
 
-
-def display_image_png_from_memory(image_bytes):
-    """Display a PNG from memory on the eInk display."""
-    # Release any previous display context
-    # displayio.release_displays()
+def display_image_bmp_from_memory(image_bytes):
+    """Display a BMP from memory on the eInk display."""
 
     # The built-in display (eInk on the MagTag)
-    # display = board.DISPLAY
     display = magtag.graphics.display
 
     # Create a main Group
@@ -58,7 +46,12 @@ def display_image_png_from_memory(image_bytes):
 
     # Wrap the image bytes in a BytesIO, then load
     with io.BytesIO(image_bytes) as f:
-        bitmap, palette = png_load.load(
+        # bitmap, palette = png_load.load(
+        #     f,
+        #     bitmap=displayio.Bitmap,       # Required
+        #     palette=displayio.Palette      # Optional if you want a palette
+        # )
+        bitmap, palette = bmp_load.load(
             f,
             bitmap=displayio.Bitmap,       # Required
             palette=displayio.Palette      # Optional if you want a palette
@@ -71,7 +64,7 @@ def display_image_png_from_memory(image_bytes):
     # Show the group on the display
     display.root_group = group
     display.refresh()
-    print("PNG displayed from memory!")
+    print("bmp displayed from memory!")
 
 while True:
     
@@ -86,15 +79,15 @@ while True:
     
       # 2) Compare to stored value
       if new_update != previous_update:
-          print("Update found! Downloading PNG to memory...")
+          print("Update found! Downloading BMP to memory...")
 
-          # 3) Download the PNG file into memory
+          # 3) Download the BMP file into memory
           img_resp = requests.get(IMAGE_URL)
-          image_data = img_resp.content  # Raw bytes of the PNG
+          image_data = img_resp.content  # Raw bytes of the BMP
           img_resp.close()
 
-          # 4) Display the newly downloaded PNG
-          display_image_png_from_memory(image_data)
+          # 4) Display the newly downloaded BMP
+          display_image_bmp_from_memory(image_data)
 
           # 5) Save new timestamp
           previous_update = new_update
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
+import { dirname, join } from "path";
+import { Jimp, JimpMime } from "jimp";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -18,8 +19,24 @@ app.get("/task", (req, res) => {
   res.json({ name: "Update code", status: "active" });
 });
 
-app.get("/image.png", (req, res) => {
-  res.sendFile("./logo.png", { root: __dirname });
+app.get("/image.png", async (req, res) => {
+  try {
+    res.sendFile("./logo.png", { root: __dirname });
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.get("/image.bmp", async (req, res) => {
+  try {
+    const image = await Jimp.read(join(__dirname, "task.png"));
+    image.posterize(2);
+    const buffer = await image.getBuffer(JimpMime.bmp, { colors: 2 });
+    res.contentType("bmp");
+    res.send(buffer);
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 app.listen(5002, () => {
